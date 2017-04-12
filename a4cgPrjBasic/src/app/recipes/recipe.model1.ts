506e4febed5d0a263f72b69c6f63783930fe3741
timchen777 <timchen7@yahoo.com>
2017-04-12 08:39:25 +0000

    48. Outputting a List of Recipes with ngFori git add .OKgit add .

--- a4cgPrjBasic/src/app/recipes/recipe.model1.ts
+++ a4cgPrjBasic/src/app/recipes/recipe.model1.ts
@@ -2,10 +2,10 @@ export class Recipe {
   public name: string;
   public description: string;
   public imagePath: string;
-
+  
   constructor(name: string, desc: string, imagePath: string) {
-    this.name = name;
-    this.description = desc;
-    this.imagePath = imagePath;
+      this.name = name;
+      this.description = desc;
+      this.imagePath = imagePath;
   }
 }
